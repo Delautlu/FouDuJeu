6de004ec035941057ddc2bfdbe73a1ca209cd0a8
SabrarLucas <pinchonlucas03@gmail.com>
2023-01-17 09:58:30 +0100

    modif

--- playsT.php
+++ playsT.php
@@ -1,34 +1,8 @@
 <?php include("head.php");?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php include("footer.php");?>
-<?php
+<div class="banner">
+    <h1 class="banner-head">
+    <video autoplay="true" loop  width="600">
+        <source src="assets/img/Foudejeux.mp4"/>
+    </video>
+    </h1>
+</div> 
