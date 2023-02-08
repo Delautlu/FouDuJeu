8c9deb0a56b4742b03c5db61848672db73337879
Delautlu <luciedelautel@outlook.fr>
2023-02-08 14:56:37 +0100

    modi08

--- head.php
+++ head.php
@@ -20,7 +20,7 @@
 <header>
 <div class="banner">
     <h1 class="banner-head">
-    <video autoplay="true" loop  width="600">
+    <video autoplay="true" loop  width="50%">
         <source src="assets/img/Foudejeux.mp4"/>
     </video>
     </h1>
